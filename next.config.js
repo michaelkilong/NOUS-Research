ad4a3ea9cdd09fe170cda24536cf550a6266d755
Michael Kilong <michaelkilongz@gmail.com>
2026-06-13 08:14:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ["res.cloudinary.com"],
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
